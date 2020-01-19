bb78f43000b97cdbcc1dc348a067870207687506
Arek <arkowalczyk95@gmail.com>
2020-01-19 21:10:42 +0100

    Table printing

--- app.R
+++ app.R
@@ -5,8 +5,7 @@ require(readr)
 
 
 data <- read_csv("CES11.csv")
-data <- subset(data, select=-c(X1, id, weight))
-summary(data)
+data <- subset(data, select=-c(X1, id, weight, population))
 data [data == 'ON'] <- "Ontario"
 data [data == 'QC'] <- "Quebec"
 data [data == 'NS'] <- "Nova Scotia"
@@ -17,9 +16,11 @@ data [data == 'PE'] <- "Prince Edward Island"
 data [data == 'SK'] <- "Saskatchewan"
 data [data == 'AB'] <- "Alberta"
 data [data == 'NL'] <- "Newfoundland and Labrador"
-
 data[] <- lapply(data, factor)
-summary(data)
+data2 <- data
+data2 <- subset(data, select=-c(abortion))
+data2$overall <- data$abortion
+data2 <- data2[,c(6,1,2,3,4,5)]
 
 library(maps)
 library(mapproj)
@@ -30,47 +31,27 @@ library(sp)
 library(raster)
 library(rgdal)
 
-can1<-getData('GADM', country="CAN", level=1) # provinces
-plot(can1)
-#spplot(can1, "NAME_1")
-# Define UI for application that draws a histogram
+#can1<-getData('GADM', country="CAN", level=1) # provinces
+#plot(can1)
+
 ui <- fluidPage(
-
-    # Application title
     titlePanel("2011 Canadian National Election Study, With Attitude Toward Abortion"),
-
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            #selectInput("inCategory", "Choose category:", choices = colnames(data2))
+            selectInput("inProvince", "Province:", choices = data$province)
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           tableOutput("provinceTable")
         )
     ),
-    #show region selection
-    selectInput(inputId = 1,label ="Region", data("AB", "BC", "MB", "NB", "NL", "NS", "ON", "PE", "QC", "SK"))
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    data <- read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/carData/CES11.csv")
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$provinceTable <- renderTable({
+        
+        provinceFilter <- subset(data, data$province == input$inProvince)
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
