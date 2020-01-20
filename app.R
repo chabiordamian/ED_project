49bf1271c2d942dc36d2a2aff8c2c75d373cddb6
Arek <arkowalczyk95@gmail.com>
2020-01-20 23:28:04 +0100

    vesrion 0.1 working

--- app.R
+++ app.R
@@ -36,10 +36,16 @@ library(leaflet.extras)
 
 sum <- table(data2$overall)[1] + table(data2$overall)[2]
 sum <- as.numeric(sum)
+
+#pie(table(data2$overall), labels = lbls, main ="Should abortion be banned?")
 lbls <- paste(names(table(data2$overall)), (round(table(data2$overall)/sum*100, digits = 2)), "%")
-pie(table(data2$overall), labels = lbls, main ="Should abortion be banned?")
+lbls2 <- paste(names(table(data2$province)), (round(table(data2$province)/sum*100, digits = 2)), "%")
+lbls3 <- paste(names(table(data2$gender)), (round(table(data2$gender)/sum*100, digits = 2)), "%")
+lbls4 <- paste(names(table(data2$importance)), (round(table(data2$importance)/sum*100, digits = 2)), "%")
+lbls5 <- paste(names(table(data2$education)), (round(table(data2$education)/sum*100, digits = 2)), "%")
+lbls6 <- paste(names(table(data2$urban)), (round(table(data2$urban)/sum*100, digits = 2)), "%")
 
-
+levels((data2$importance))
 
 if (!file.exists("./src/ref/ne_50m_admin_1_states_provinces_lakes/ne_50m_admin_1_states_provinces_lakes.dbf")){
     download.file(file.path('http://www.naturalearthdata.com/http/',
@@ -51,22 +57,25 @@ if (!file.exists("./src/ref/ne_50m_admin_1_states_provinces_lakes/ne_50m_admin_1
 }
 
 region <- readOGR("./src/ref/ne_50m_admin_1_states_provinces_lakes", 'ne_50m_admin_1_states_provinces_lakes', encoding='UTF-8')
+levels(region$name)[72] <- "Quebec"
+levels(region$name)[72]
 
 ui <- fluidPage(
     titlePanel("2011 Canadian National Election Study, With Attitude Toward Abortion"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("inCategory", "Choose category:", choices = colnames(data2)),
-            selectInput("inProvince", "Province:", choices = data$province)
+            selectInput("inCategory", "Choose a category for pie chart:", choices = colnames(data2)),
+            #selectInput("inGroup", "Choose a group for pie chart:", choices = levels(data2$inCategory)),
+            selectInput("inProvince", "Province for Map and Table:", choices = data$province)
         ),
         mainPanel(
             # Output: Tabset w/ plot, summary, and table ----
             tabsetPanel(type = "tabs",
-                        tabPanel("Plot", plotOutput("overallChart")),
-                        tabPanel("Map", leafletOutput(outputId = "mymap")),
-                        tabPanel("Table", tableOutput("provinceTable")))
+                        tabPanel("Pie chart results", plotOutput("overallChart")),
+                        tabPanel("Map of Provinces", leafletOutput(outputId = "mymap")),
+                        tabPanel("Results for particular province", tableOutput("provinceTable")))
         )
-    ),
+    )
 )
 
 server <- function(input, output) {
@@ -75,8 +84,6 @@ server <- function(input, output) {
         provinceFilter <- subset(data, data$province == input$inProvince)
     })
     
-
-
     #create the map
     output$mymap <- renderLeaflet({leaflet() %>%
             addTiles() %>% 
@@ -99,7 +106,32 @@ server <- function(input, output) {
     observeEvent(input$inCategory, 
         if(input$inCategory == 'overall'){
             output$overallChart <- renderPlot({
-                pie(table(data2$overall), labels = lbls, main =paste0(input$inCategory,"\n Should abortion be banned?"))
+                pie(table(data2$overall), labels = lbls, main =paste0("Category: ",input$inCategory,"\n Should abortion be banned?"))
+            })
+        }
+        else if(input$inCategory == 'province'){
+            output$overallChart <- renderPlot({
+                pie(table(data2$province), labels = lbls2, main =paste0("Category: ",input$inCategory,"\n Participants per province"))
+            })
+        }
+        else if(input$inCategory == 'gender'){
+            output$overallChart <- renderPlot({
+                pie(table(data2$gender), labels = lbls3, main =paste0("Category: ",input$inCategory,"\n Participants per gender"))
+            })
+        }
+        else if(input$inCategory == 'importance'){
+            output$overallChart <- renderPlot({
+                pie(table(data2$importance), labels = lbls4, main =paste0("Category: ",input$inCategory,"\n Is the subject of abortion important?"))
+            })
+        }
+        else if(input$inCategory == 'education'){
+            output$overallChart <- renderPlot({
+                pie(table(data2$education), labels = lbls5, main =paste0("Category: ",input$inCategory,"\n Participants per education"))
+            })
+        }
+        else if(input$inCategory == 'urban'){
+            output$overallChart <- renderPlot({
+                pie(table(data2$urban), labels = lbls6, main =paste0("Category: area\n Participants per area"))
             })
         }
     )
